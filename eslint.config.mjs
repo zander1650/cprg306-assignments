e0b48ab6f7cfa7e16c8d10e5c8262091a7076c9f
Zander Fields <zander28fields@gmail.com>
2026-08-04 14:11:29 -0600

    Fix syntax errors in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-sdaasdaadsads
+sdaasdaadsadss
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
