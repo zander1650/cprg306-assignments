b179721520dc387b25f2985b9aea0bc9b79b094c
Zander Fields <zander28fields@gmail.com>
2026-08-04 14:12:06 -0600

    Fix typo in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-sdaasdaadsadssssss
+sdaasdaadsadsssssss
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
